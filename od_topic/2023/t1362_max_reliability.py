dfacf3fd3a0a542a43c701f69e40425c4bed280a
pengxiaozhe <728100660@qq.com>
2023-08-23 14:00:02 +0800

    fix:元件最大可靠性-枚举-有点难，-注意边界条件，初始条件

--- od_topic/2023/t1362_max_reliability.py
+++ od_topic/2023/t1362_max_reliability.py
@@ -69,18 +69,72 @@ N种器件，则返回
 组成设备需要
 1个元件，但是元件价格大于预算，因此无法组成设备，返回
 −1
+
+
+10 10
+10
+0 63202 0
+1 815 2
+9 2721 2
+7 81610 0
+1 58899 2
+1 36780 2
+8 90407 2
+2 17837 0
+5 47874 0
+1 66133 1
 """
 
 """
-枚举
+枚举：对可靠性进行递归，选择符合条件的最大可靠性值，每次枚举选择满足要求的最便宜的元件
+首先判断给出元件种类数是否满足要求，不满足返回-1
+用二分法优化时间复杂度
+预算不足：减少可靠性要求，左半边
+预算足够：记录结果，继续增加可靠性要求，看看有没有更大可靠性的
 """
 
 def main():
     total_price, type_num = tuple(map(int, input().split()))
     type_map = {}       # 种类信息映射表
-    component_num = int(input())
+    component_num = int(input())        # 元件总数
     for _ in range(component_num):
         type1, reliability, price = tuple(map(int, input().split()))
         if type1 not in type_map:
             type_map[type1] = []
         type_map[type1].append({"r": reliability, "price": price})
+
+    # 判断输入合理性
+    if len(type_map) < type_num:            # len(type_map)不可能大于type_num
+        return -1
+
+    # 枚举
+    res_reliability = -1
+    left = 0
+    right = 100000
+    while left <= right:
+        mid_reliability = (left + right) // 2
+        min_reliability = float("inf")      # 此次满足需求的最低可靠性设备的可靠性
+        cur_price = 0
+        tmp = []
+        for type1, info_list in type_map.items():
+            min_price = float("inf")
+            tmp.append(max([info["r"] for info in info_list]))
+            for info in info_list:
+                if info["r"] < mid_reliability: # 不满足要求
+                    continue
+                if info["price"] > min_price:   # 价格太高
+                    continue
+                tmp_reliability = info["r"]
+                min_reliability = min(min_reliability, tmp_reliability)
+                min_price = info["price"]
+
+            cur_price += min_price  # 记录当前种类花费
+        if cur_price > total_price:  # 超预算  左移
+            right = mid_reliability - 1
+        else:       # 符合预算，右移
+            res_reliability = max(min_reliability, res_reliability)
+            left = mid_reliability + 1
+    return res_reliability
+
+
+print(main())
